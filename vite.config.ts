f798e67cdfd01180b188ad2b7b016dd9f5d6e1eb
trainingitspec <helpzsu.ua@gmail.com>
2026-05-22 19:33:25 +0300

    fix: Створення файлу декларації типів

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-  // Тут можуть бути ваші плагіни, наприклад: plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // Наказуємо Vite розпізнавати "@" як шлях до папки "src"
+      '@': resolve(__dirname, './src'),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),      // Головний сайт
+        main: resolve(__dirname, 'index.html'),       // Головний сайт
         admin: resolve(__dirname, 'public/admin/index.html') // Ваша адмінка
       }
     }
